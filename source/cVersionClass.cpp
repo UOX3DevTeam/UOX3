7e892dbf6ad55bba742124f72f481f238ee7fded
Matthew Randall <mrandall@adtastik.net>
2002-10-25 05:43:46 +0000

    This is the cVersionClass object. The ide was to start moving things that were not really needed directly in the uox3.h file so that compile time would be lowered some. and eventually dramatically..

---
+++ source/cVersionClass.cpp
@@ -0,0 +1,282 @@
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+#include "cVersionClass.h"
+
+//o---------------------------------------------------------------------------o
+// product info
+//o---------------------------------------------------------------------------o
+#define VER "0.97.01"
+#define BUILD "16a"
+#define REALBUILD "0"
+#define SVER "2.0"
+#define CVER "2.0"
+#define IVER "2.0"
+#define PRODUCT "Ultima Offline eXperiment 3"
+#define NAME "The Official DevTeam"
+#define EMAIL "http://uox3.sourceforge.net/"
+#define PROGRAMMERS "UOX3 DevTeam"
+
+cVersionClass CVC;
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+cVersionClass::cVersionClass()
+{
+	SetVersion(VER);
+	SetBuild(BUILD);
+	SetRealBuild(REALBUILD);
+	SetScriptVersion(SVER);
+	SetItemVersion(CVER);
+	SetCharacterVersion(IVER);
+	SetProductName(PRODUCT);
+	SetName(NAME);
+	SetProgrammers(EMAIL);
+	SetEmail(PROGRAMMERS);
+}
+//o---------------------------------------------------------------------------o
+cVersionClass::~cVersionClass()
+{
+
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetVersion(char *szVersion)
+{
+	strncpy(szIVersion,szVersion,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetVersion(void)
+{
+	return szIVersion;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetBuild(char *szBuild)
+{
+	strncpy(szIBuild,szBuild,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetBuild(void)
+{
+	return szIBuild;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetRealBuild(char *szRealBuild)
+{
+	strncpy(szIRealBuild,szRealBuild,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetRealBuild(void)
+{
+	return szIRealBuild;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetScriptVersion(char *szScriptVersion)
+{
+	strncpy(szIScriptsVersion,szScriptVersion,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetScriptVersion(void)
+{
+	return szIScriptsVersion;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetCharacterVersion(char *szCharacterVersion)
+{
+	strncpy(szICharactersVersion,szCharacterVersion,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetCharacterVersion(void)
+{
+	return szICharactersVersion;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetItemVersion(char *szItemVersion)
+{
+	strncpy(szIItemsVersion,szItemVersion,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetItemVersion(void)
+{
+	return szIItemsVersion;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetProductName(char *szProduct)
+{
+	strncpy(szIProductName,szProduct,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetProductName(void)
+{
+	return szIProductName;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetName(char *szName)
+{
+	strncpy(szIName,szName,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetName(void)
+{
+	return szIName;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetEmail(char *szEmail)
+{
+	strncpy(szIEmail,szEmail,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetEmail(void)
+{
+	return szIEmail;
+}
+
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+void cVersionClass::SetProgrammers(char *szProgrammers)
+{
+	strncpy(szIProgrammers,szProgrammers,sizeof(char)*32);
+}
+//
+char *cVersionClass::GetProgrammers(void)
+{
+	return szIProgrammers;
+}
---
+++ source/cVersionClass.h
@@ -0,0 +1,63 @@
+//o--------------------------------------------------------------------------o
+//|	Function/Class-	
+//|	Date					-	10/17/2002
+//|	Developer(s)	-	EviLDeD
+//|	Company/Team	-	UOX3 DevTeam
+//|	Status				-	
+//o--------------------------------------------------------------------------o
+//|	Description		-	
+//o--------------------------------------------------------------------------o
+//|	Returns				-
+//o--------------------------------------------------------------------------o
+//|	Notes					-	
+//o--------------------------------------------------------------------------o	
+#ifndef __CVERSIONCLASS_H__
+#define __CVERSIONCLASS_H__
+
+#include <string.h>
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class cVersionClass  
+{
+public:
+	cVersionClass();
+	virtual ~cVersionClass();
+	// Need to set our values for the system. Pair matching Get routine as well(script modification connection)
+	virtual void SetVersion(char *szVersion);
+	char *GetVersion(void);
+	virtual void SetBuild(char *szBuild);
+	char *GetBuild(void);
+	virtual void SetRealBuild(char *szRealBuild);
+	char *GetRealBuild(void);
+	virtual void SetScriptVersion(char *szScriptVersion);
+	char *GetScriptVersion(void);
+	virtual void SetCharacterVersion(char *szCharacterVersion);
+	char *GetCharacterVersion(void);
+	virtual void SetItemVersion(char *szItemVersion);
+	char *GetItemVersion(void);
+	virtual void SetProductName(char *szProduct);
+	char *GetProductName(void);
+	virtual void SetName(char *szName);
+	char *GetName(void);
+	virtual void SetEmail(char *szEmail);
+	char *GetEmail(void);
+	virtual void SetProgrammers(char *szProgrammers);
+	char *GetProgrammers(void);
+
+public:
+	char szIVersion[32];
+	char szIBuild[32];
+	char szIRealBuild[32];
+	char szIScriptsVersion[32];
+	char szICharactersVersion[32];
+	char szIItemsVersion[32];
+	char szIProductName[80];
+	char szIEmail[128];
+	char szIProgrammers[128];
+	char szIName[80];
+};
+
+#endif // __CVERSIONCLASS_H__
