3edb9f544468423ae3193ece7bb88bef10941dde
Matthew Randall <mrandall@adtastik.net>
2003-03-04 14:08:41 +0000

    *** empty log message ***

--- source/classes.h
+++ source/classes.h
@@ -13,9 +13,6 @@ class CMultiObj;
 #include "cBaseObject.h"
 
 
-typedef std::vector< CChar * > CHARLIST;
-typedef std::vector< CItem * > ITEMLIST;
-
 #include "cChar.h"
 #include "cItem.h"
 
@@ -36,24 +33,23 @@ public:
 	void		GlowItem( CItem *i );
 	CItem *		MemItemFree( ITEM& offset, bool zeroSer, UI08 itemType = 0 );
 	void		DeleItem( CItem *i );
+	void		menuAddItem( cSocket *s, std::string item );
 	CItem *		CreateScriptItem( cSocket *s, std::string name, bool nSpawned, UI08 worldNumber );
-	CItem *		CreateRandomItem( cSocket *s, const char *sItemList, bool nSpawned, UI08 worldNumber );
-	CItem *		SpawnItem( cSocket *nSocket, UI32 nAmount, const char *cName, bool nStackable, UI16 realItemId, UI16 realColour, bool nPack, bool nSend );
 	CItem *		SpawnItem( cSocket *nSocket, CChar *ch, UI32 nAmount, const char *cName, bool nStackable, UI16 realItemID, UI16 realColour, bool nPack, bool nSend );
-	CItem *		SpawnItemToPack( cSocket *s, CChar *ch, int nItem, bool nDigging );
-	CItem *		SpawnItemToPack( cSocket *s, CChar *ch, std::string name, bool nDigging );
-	CMultiObj *	SpawnMulti( cSocket *nSocket, CChar *ch, const char *cName, UI16 realItemID );
-	void		GetScriptItemSetting( CItem *c );
+	CItem *		SpawnItemToPack( cSocket *s, CChar *mChar, int nItem, bool nDigging );
+	CItem *		SpawnItemToPack( cSocket *s, CChar *mChar, std::string name, bool nDigging );
+	CMultiObj *	SpawnMulti( CChar *ch, const char *cName, UI16 realItemID );
 	bool		DecayItem( CItem *i );
-	void		Decay( void );
 	void		RespawnItem( CItem *i );
 	void		AddRespawnItem( CItem *s, UI32 x, bool inCont );
 	void		AddRespawnItem( CItem *s, const char *x, bool inCont, bool randomItem );
-	UI08		PackType( UI16 id );
 	void		CheckEquipment( CChar *p );
 
 private:
 	CItem *		CreateItem( cSocket *s, std::string name, UI08 worldNumber );
+	CItem *		CreateRandomItem( cSocket *s, const char *sItemList, bool nSpawned, UI08 worldNumber );
+	void		GetScriptItemSetting( CItem *c );
+	UI08		PackType( UI16 id );
 };
 
 // use this value whereever you need to return an illegal z value
@@ -63,23 +59,23 @@ class cCharStuff
 {
 private:
 	void		FindSpotForNPC( CChar *c, SI16 originX, SI16 originY, SI16 xAway, SI16 yAway, SI08 z );
-	CChar *		CreateScriptNpc( cSocket *s, std::string targNPC, UI08 worldNumber );
-	CChar *		CreateScriptNpc( cSocket *s, int    targNPC, UI08 worldNumber );
+	CChar *		CreateScriptNpc( std::string targNPC, UI08 worldNumber );
+	CChar *		CreateScriptNpc( int    targNPC, UI08 worldNumber );
 	bool		ApplyNpcSection( CChar *applyTo, ScriptSection *NpcCreation );
 	void		PostSpawnUpdate( CChar *x );
+	void		LoadShopList( const char *list, CChar *c );
 
 public:
 	void		DeleteChar( CChar *k );
 	CChar *		MemCharFree( CHARACTER& offset, bool zeroSer = false );
 	CItem *		addRandomLoot( CItem *s, const char * lootlist );
-	CChar *		CreateRandomNpc( cSocket *s, const char *npclist, UI08 worldNumber );
+	CChar *		CreateRandomNpc( const char *npclist, UI08 worldNumber );
 	CChar *		SpawnNPC( cSpawnRegion *spawnReg, std::string npcNum, UI08 worldNumber );
 	CChar *		SpawnNPC( CItem *i, std::string npcNum, UI08 worldNumber, bool randomNPC );
 	CChar *		AddNPC( cSocket *s, cSpawnRegion *spawnReg, std::string npcNum, UI08 worldNumber );
 	CChar *		AddNPCxyz( cSocket *s, std::string npcNum, SI16 x1, SI16 y1, SI08 z1, UI08 worldNumber );
 	void		Split( CChar *k );
 	void		CheckAI( CChar *i );
-	void		LoadShopList( const char *list, CChar *c );
 	CChar *		getGuardingPet( CChar *mChar, SERIAL guarded );
 	bool		checkPetFriend( CChar *mChar, CChar *pet );
 	void		stopPetGuarding( CChar *pet );
@@ -92,6 +88,7 @@ public:
 #include "cMagic.h"
 #include "movement.h"
 #include "teffect.h"
+#include "packets.h"
 #include "gump.h"
 #include "PageVector.h"
 #include "cMultiObj.h"
