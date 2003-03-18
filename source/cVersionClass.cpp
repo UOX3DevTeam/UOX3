736198c6e40102d3bc12e6d1acd9e8d5ed556039
Matthew Randall <mrandall@adtastik.net>
2003-03-18 08:18:16 +0000

    Ewps just relized forgot to commit this file too :) v0.97.3.1i thanks Phil, Malketh, Zane, the everyone list..

--- source/cVersionClass.cpp
+++ source/cVersionClass.cpp
@@ -17,7 +17,7 @@
 // product info
 //o---------------------------------------------------------------------------o
 #define VER "0.97.03"
-#define BUILD "1h"
+#define BUILD "1i"
 #define REALBUILD "0"
 #define SVER "2.0"
 #define CVER "2.0"
