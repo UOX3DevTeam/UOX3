f4fa4ca03e2b958e0b7209f47f6808ed283b54c0
Matthew Randall <mrandall@adtastik.net>
2003-03-27 04:06:56 +0000

    Thanks to dreoth on this one. The docs at sf will be updated. now you should be able to us this shell script and it will automate the processes to compliling the javascrip lib's as well as configure, and make for uox3.. Thanks Dreoth!!

---
+++ source/autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "aclocal"
+aclocal || exit;
+echo "automake --ad-missing --copy"
+automake --add-missing --copy;
+echo "autoconf"
+autoconf || exit;
+echo "automake"
+automake || exit;
+
+echo "Building required linux file js32.a"
+
+cd ./mozilla/js/src;
+gcc -o jscpucfg jscpucfg.c ; ./jscpucfg >  jsautocfg.h
+gcc -c -DXP_UNIX *.c ; ar -r js32.a *.o
+cp js32.a ../../../;
+cd ../../../;
+
+echo "Done building js32.a"
+
+echo "Running configure."
+./configure $@
+echo "Please do ./configure --help to set options such as --enable-debug"
+echo "You can also run make now to compile UOX3. Enjoy!"
