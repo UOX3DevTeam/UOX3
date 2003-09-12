d1b08a8b74f4008717cbc9b05d82c0791c1d00a1
Daniel Stratton <dsstratton@outlook.com>
2003-09-12 10:17:22 +0000

    Updates to use std::string instead of char *, to make the logic look simpler 	Added a UString class that supports stripping of whitespace, comments and other commonly used functions on strings 	Implemented some use of UString already, simplifying some of our string parsing

---
+++ source/ustring.cpp
@@ -0,0 +1,766 @@
+#if defined(_MSC_VER) 
+	#if _MSC_VER < 1300
+		#pragma warning( disable : 4786 )
+		#pragma warning( disable : 4514 )
+		#pragma warning( disable : 4097 )
+		#define UOXVALIST va_list
+		#define NOSIZETYPE
+	#else
+		#define UOXVALIST std::va_list
+	#endif
+#else
+	#define UOXVALIST std::va_list
+#endif
+
+#include "ustring.h" 
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(): stdstring() 
+{
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(const std::string& str) : stdstring(str)
+{
+}
+#if !defined( NOSIZETYPE )
+UString::UString( const std::string& str, std::string::size_type str_idx ) : stdstring( str, str_idx )
+{
+} 
+#endif
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(const std::string& str, std::string::size_type str_idx, 
+				 std::string::size_type str_num) : stdstring(str,str_idx,str_num)
+{
+} 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(const char* cstr) : stdstring(cstr)
+{
+} 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(const char* chars, std::string::size_type chars_len) :
+stdstring(chars,chars_len)
+{
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString::UString(std::string::size_type num, char c) : stdstring(num,c)
+{
+} 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::stripWhiteSpace() const
+{
+	// Eliminate any leading white space
+	UString data  = static_cast<UString>((*this));
+	size_t offset = (*this).find_first_not_of(MYWHITESPACE) ;
+	if ( offset != std::string::npos )
+	{
+		data = (*this).substr(offset) ;
+	}
+	offset = data.find_last_not_of(MYWHITESPACE) ;
+	if ( offset!=std::string::npos )
+	{
+		data = data.substr(0,offset+1) ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
+UString UString::simplifyWhiteSpace() const
+{
+	UString working = (*this).stripWhiteSpace() ;
+	UString data ="" ;
+	//std::string::iterator iter ;
+	//iter = working.begin() ;
+	bool first = false ;
+	char value[2] ;
+	value[1] = 0 ;
+	for ( unsigned int i = 0 ; i < working.length() ;i++ )
+	{
+		value[0] = working[i] ;
+		std::string  test = value ;
+		if ( test.find_first_of(MYWHITESPACE) != std::string::npos )
+		{
+			if ( !first )
+			{
+				data = (stdstring)(data) + test;
+				first = true ;
+			}
+
+		}
+		else
+		{
+			first = false ;
+			data = data + value ;
+		}
+	}
+
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::section(const char* sep,int start,int stop)
+{
+	std::string temp = sep ;
+	return(section(temp,start,stop) );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::section(std::string sep,int start,int stop)
+{
+	UString data ;
+	int count = -1 ;
+	size_t startoffset = 0 ;
+	size_t stopoffset = 0 ;
+	bool match = false ;
+	stopoffset = (*this).find(sep,startoffset);
+	while ( startoffset != std::string::npos )
+	{
+		count++ ;
+		if ( count == start )
+		{
+			match = true ;
+			// We dont return the seperator, so jump over it
+			size_t tempoffset = startoffset ;
+			while ( stopoffset != std::string::npos )
+			{
+				if ( count == stop )
+				{
+					break ;
+				}
+				else
+				{
+					tempoffset = stopoffset + sep.length() ;
+					stopoffset = (*this).find(sep,tempoffset) ;
+					count++ ;
+				}
+			}
+			break ;
+
+		}
+		else
+		{
+			if ( stopoffset != std::string::npos )
+				startoffset = stopoffset+ sep.length();
+			else
+				startoffset	= static_cast<size_t>(std::string::npos) ;
+			stopoffset = (*this).find(sep,startoffset) ;
+
+		}
+	}
+	if ( match )
+	{
+		size_t length = stopoffset - startoffset ;
+		if ( stopoffset == std::string::npos )
+			length = (*this).length() - startoffset ;
+
+		data = (*this).substr(startoffset,length) ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( float n )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	input >> data ;
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( double n )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	input >> data ;
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( short n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( unsigned short n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( int n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( unsigned int n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( long n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString  UString::setNum ( unsigned long n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//UString & UString::setNum ( float n, char f = 'g', int prec = 6 ) ;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::number (float n)
+{
+	UString data ;
+	std::stringstream input ;
+	input << n ;
+	input >> data ;
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::number (double n)
+{
+	UString data ;
+	std::stringstream input ;
+	input << n ;
+	input >> data ;
+	return(data );
+}
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::number ( long n, int base  ) 
+{
+
+	UString data ;
+
+	std::stringstream input ;
+	input << n ;
+
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::number ( unsigned long n, int base )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::number ( int n, int base  )
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+UString UString::number ( unsigned int n, int base  ) 
+{
+	std::stringstream input ;
+	input << n ;
+	UString data ;
+	switch ( base )
+	{
+		case 10:
+			input >> std::dec >> data ;
+			break ;
+		case 16:
+			input >> std::hex >> data ;
+			break ;
+		case 8:
+			input >> std::oct>> data ;
+			break ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::lower() const
+{
+	UString sReturn = *this; 
+	std::transform(sReturn.begin(), sReturn.end(), sReturn.begin(), ::tolower);
+	return(sReturn);
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::upper() const
+{
+	UString sReturn = *this;
+
+	std::transform(sReturn.begin(), sReturn.end(), sReturn.begin(), ::toupper);
+	return(sReturn);
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+float UString::toFloat(bool * ok) const
+{
+	float data ;
+	std::stringstream input ;
+	input << *this ;
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+double UString::toDouble(bool * ok)  const  
+{
+	double data ;
+	std::stringstream input ;
+	input << *this ;
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+short UString::toShort ( bool * ok , int base  ) const 
+{
+	short data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+	return(data );
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+unsigned short UString::toUShort ( bool * ok , int base  ) const
+{
+	unsigned short data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+int UString::toInt ( bool * ok , int base  ) const
+{
+	int data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+	return(data );
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+unsigned int UString::toUInt ( bool * ok , int base ) const
+{
+	unsigned int data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+long UString::toLong ( bool * ok , int base  ) const
+{
+	long data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+	return(data );
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+unsigned long UString::toULong ( bool * ok , int base  ) const
+{
+	unsigned long data ;
+	std::stringstream input ;
+	switch ( base )
+	{
+		case -1:
+			size_t offset ;
+			if ( (offset = (*this).find("0X")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else if ( (offset = (*this).find("0x")) != std::string::npos )
+			{
+				input << std::hex <<(*this).substr(offset+2) ;
+			}
+			else
+				input << std::dec <<(*this)	;
+			break ;
+		case 10:
+			input <<std::dec << (*this) ;
+			break ;
+		case 16:
+			input << std::hex << (*this) ;
+			break ;
+		case 8:
+			input << std::oct << (*this) ;
+			break ;
+	}
+	input >> data ;
+	if ( ok != NULL )
+		(*ok) = input.good() ;
+
+	return(data );
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::comment() const
+{
+	UString data ;
+	std::string sep = COMMENTTAG ;
+	size_t offset = find(sep) ;
+	if ( offset != std::string::npos )
+	{
+		offset = offset + sep.length() ;
+		data = (*this).substr(offset) ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::removeComment() const
+{
+	UString data = *this;
+	std::string sep = COMMENTTAG ;
+	size_t offset = find(sep) ;
+	if ( offset != std::string::npos )
+	{
+		data = (*this).substr(0,offset) ;
+	}
+	return(data );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+std::string UString::replaceSlash(const std::string data)
+{
+	std::string value = "\\" ;
+	std::string::size_type index = 0 ;
+	std::string input = data ;
+
+	while ( (index = input.find(value,index)) != std::string::npos )
+	{
+		input = input.replace(index,1,"/") ;
+	}
+	return(input );
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::fixDirectory() 
+{
+	UString temp = *this;
+	temp = temp.stripWhiteSpace() ;
+	temp = replaceSlash(temp) ;
+	if ( temp.find_last_of("/") != temp.size()-1 )
+	{
+		temp = temp+"/" ;
+	}
+	return (temp );
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::operator+(const char * input ) 
+{
+	UString temp(input) ;
+	UString base = *this ;
+//	return base + temp ;
+	return (stdstring)base + (stdstring)temp;
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UString UString::sprintf(const char* format,...)
+{
+	char buffer[2048] ;	// yes, we are limiting to 2k buffers 
+	memset(buffer,0,2048) ;
+
+	UOXVALIST marker;
+	va_start( marker, format ) ;
+#if !defined(__unix__)
+	_vsnprintf( buffer, 2048, format, marker );
+#else
+	vsnprintf( buffer, 2048, format, marker );
+#endif
+	va_end( marker );			   // Reset variable arguments.  
+	UString status = buffer ;
+
+
+	return(status );
+}
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+int UString::sectionCount(std::string sep) 
+{
+	int count = -1 ;
+
+	UString temp =(*this).simplifyWhiteSpace()  ;
+
+	size_t index = temp.find(sep) ;
+	while ( index != std::string::npos )
+	{
+		count++ ;
+		index = temp.find(sep,index+1) ;
+	}
+
+	count++ ;
+	return(count );
+}
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+int UString::sectionCount(const char* sep)
+{
+	return(sectionCount(std::string(sep)) );
+}
---
+++ source/ustring.h
@@ -0,0 +1,101 @@
+#ifndef __STRING_H
+#define __STRING_H
+// Ssytem Includes
+#include <string>
+#include <cstdarg>
+#include <cstdio> 
+#include <sstream>
+#include <algorithm>
+
+
+typedef std::string stdstring ;
+
+// Defines
+
+#define MYWHITESPACE " \t\v\f\0 "
+#define COMMENTTAG "//"
+
+//#define MYNONWHITESPACE "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=`~!@#$%^&*()_+|,<.>/?;:'[{]}\"\\"
+//! String class with extensions
+/*! This string class mimics many of the capaiblities provided by QString in the 
+	Trolltech QT library.  It has methods to handle number conversion's, convert
+	to upper and lower, spit a string into sections based on delimitors, and 
+	remove comment strings.
+*/
+class UString : public stdstring
+{
+public:
+	//! Base constructor 
+	UString() ;
+	//! Create a UString from another string
+	UString(const std::string& str) ;
+	//! Create a UString from another string, starting at the specified index
+	UString(const std::string& str, std::string::size_type str_idx) ;
+	//! Create a UString from another string, starting at the specified index and for the specified number of characters 
+	UString(const std::string& str, std::string::size_type str_idx, std::string::size_type str_num) ;
+	//! Create a UString from a char delimited string
+	UString(const char* cstr) ;
+	//! Create a UString from a char delimted string, using the specified number of characters
+	UString(const char* chars, std::string::size_type chars_len) ;
+	//! Create a Ustring that repeats the character specified
+	UString(std::string::size_type num, char c) ;
+	//! Remove all leading, trailing white space, and all redundant white space within the string
+	UString simplifyWhiteSpace() const ;
+	//! Remove all trailing and leading white space
+	UString stripWhiteSpace() const ;
+	//! Using the specified sep as a delimitor, return the specifed section of the string
+	UString section(std::string sep, int start, int stop=std::string::npos) ;
+	//! Overloaded member of the above
+	UString section(const char* sep, int start, int stop=std::string::npos) ;
+
+	//!  How many "sections" are in this string ?
+	int sectionCount(std::string sep) ;
+	//!  How many "sections" are in this string ?
+	int sectionCount(const char* sep) ;
+
+	unsigned short toUShort ( bool * ok = 0, int base = -1 ) const;
+	short toShort ( bool * ok = 0, int base = 10 ) const ;
+
+	int toInt ( bool * ok = 0, int base = -1 ) const;
+	unsigned int toUInt ( bool * ok = 0, int base = -1 ) const;
+
+	long toLong ( bool * ok = 0, int base = -1 ) const;
+	unsigned long toULong ( bool * ok = 0, int base = -1 ) const;
+
+	float toFloat(bool * ok=0) const ;
+	double toDouble(bool * ok=0) const ;
+
+	UString  setNum ( short n, int base = 10 );
+	UString  setNum ( unsigned short n, int base = 10 );
+	UString  setNum ( int n, int base = 10 );
+	UString  setNum ( unsigned int n, int base = 10 );
+	UString  setNum ( long n, int base = 10 );
+	UString  setNum ( unsigned long n, int base = 10 );
+	UString  setNum ( float n ) ;
+	UString setNum(double n) ;
+
+	UString lower () const;
+	UString upper () const;
+
+	UString removeComment() const ;
+	UString comment() const ;
+
+	std::string replaceSlash(const std::string data);
+	UString fixDirectory()  ;
+
+	UString operator+(const char * ) ;
+
+	static UString number ( long n, int base = 10 ) ;
+	static UString number ( unsigned long n, int base = 10 ) ;
+	static UString number ( int n, int base = 10 )  ;
+	static UString number ( unsigned int n, int base = 10 )  ;
+	static UString number (float n) ;
+	static UString number (double n) ;
+	//static UString sprintf(std::string format,...) ;
+	static UString sprintf(const char* format,...) ;
+
+
+protected:
+
+};
+#endif
