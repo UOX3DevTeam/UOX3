4fff467ad4350bcc22f1420bc3e04555ad97357f
Daniel Stratton <dsstratton@outlook.com>
2004-09-07 12:44:59 +0000

    Updates from Xuri

--- js/commands/targeting/set.js
+++ js/commands/targeting/set.js
@@ -8,6 +8,12 @@ function command_SET( socket, cmdString )
 {
 	if( cmdString )
 	{
+		var splitString = cmdString.split( " ", 2 );
+		if( !splitString[1] )
+		{
+			socket.SysMessage( GetDictionaryEntry( 1755, socket.Language )); //Additional arguments required
+			return;
+		}
 		var targMsg = GetDictionaryEntry( 1741, socket.Language );
 		socket.xText = cmdString;
 		socket.CustomTarget( 0, "Choose target to set: " + cmdString );
@@ -28,6 +34,7 @@ function onCallback0( socket, ourObj )
 	{
 	case "NAME":
 		ourObj.name = socket.xText.substring( 5 );
+		okMsg( socket );
 		break;
 	case "STR":
 	case "STRENGTH":
@@ -36,23 +43,28 @@ function onCallback0( socket, ourObj )
 	case "DEX":
 	case "DEXTERITY":
 		ourObj.dexterity = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "INT":
 	case "INTELLIGENCE":
 		ourObj.intelligence = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "FAME":
 		ourObj.fame = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "KARMA":
 		ourObj.karma = StringToNum( splitString[1] );
 		break;
 	case "KILLS":
 		ourObj.murdercount = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "COLOR":
 	case "COLOUR":
 		ourObj.colour = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "OWNER":
 		socket.tempObj = ourObj;
@@ -60,9 +72,11 @@ function onCallback0( socket, ourObj )
 		break;
 	case "X":
 		ourObj.x = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	case "Y":
 		ourObj.y = StringToNum( splitString[1] );
+		okMsg( socket );
 		break;
 	default:
 		if( ourObj.isChar )
@@ -77,65 +91,85 @@ function onCallback0( socket, ourObj )
 
 function HandleSetItem( socket, ourItem, uKey, value )
 {
+	var splitString = socket.xText.split( " ", 2 );
+	uKey = splitString[0].toUpperCase();
 	switch( uKey )
 	{
 	case "ID":
 		ourItem.id = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "AMOUNT":
 		ourItem.amount = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MOVABLE":
 		ourItem.movable = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "WIPABLE":
 		ourItem.wipable = (StringToNum( value ) == 1);
+		okMsg( socket );
 		break;
 	case "BUYVALUE":
 		ourItem.buyvalue = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "SELLVALUE":
 		ourItem.sellvalue = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "RESTOCK":
 		ourItem.restock = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MORE":
 		ourItem.more = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MOREX":
 		ourItem.morex = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MOREY":
 		ourItem.morey = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MOREZ":
 		ourItem.morez = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "DEVINELOCK":
 		ourItem.devinelock = (StringToNum( value ) == 1);
+		okMsg( socket );
 		break;
 	case "DIR":
 		ourItem.dir = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "NAME2":
 		ourItem.name2 = socket.xText.substring( 6 );
+		okMsg( socket );
 		break;
 	case "TYPE":
 		ourItem.type = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "Z":
 		ourItem.z = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "DECAYABLE":
 		ourItem.decayable = (StringToNum( value ) == 1);
+		okMsg( socket );
 		break;
 	case "VISIBLE":
 		ourItem.visible = StringToNum( value );
+		okMsg( socket );
 		break;
 	default:
-		if( ourObj.isSpawner )
-			HandleSetSpawner( socket, ourObj, uKey, splitString[1] );
+		if( ourItem.isSpawner )
+			HandleSetSpawner( socket, ourItem, uKey, splitString[1] );
 		else
 			socket.SysMessage( "Invalid set command " + uKey );
 		break;
@@ -158,12 +192,15 @@ function HandleSetSpawner( socket, ourSpawn, uKey, value )
 			ourSpawn.spawnsection = value;
 			ourSpawn.sectionalist = false;
 		}
+		okMsg( socket );
 		break;
 	case "MININTERVAL":
 		ourSpawn.mininterval = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "MAXINTERVAL":
 		ourSpawn.maxinterval = StringToNum( value );
+		okMsg( socket );
 		break;
 	default:
 		socket.SysMessage( "Invalid set command " + uKey );
@@ -176,60 +213,81 @@ function HandleSetChar( socket, ourChar, uKey, value )
 	{
 	case "ALLSKILLS":
 		ourChar.baseskills.allskills = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "ID":
 		var idVal = StringToNum( value );
 		if( idVal <= 0x7CF )
+		{
 			ourChar.id = idVal;
+			okMsg( socket );
+		}
 		break;
 	case "FONT":
 		ourChar.font = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "SPATTACK":
 		ourChar.spattack = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "SPDELAY":
 		ourChar.spdelay = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "POISON":
 		ourChar.poison = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "TITLE":
 		ourChar.title = socket.xText.substring( 6 );
+		okMsg( socket );
 		break;
 	case "NPCWANDER":
 		ourChar.wantertype = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "DIR":
 		ourChar.dir = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "NPCAI":
 		ourChar.aitype = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "VULNERABLE":
 		ourChar.vulnerable = (StringToNum( value ) == 1);
+		okMsg( socket );
 		break;
 	case "SPLIT":
 		ourChar.split = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "SPLITCHANCE":
 		ourChar.splitchance = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "COMMANDLEVEL":
 		ourChar.commandlevel = StringToNum( value );
+		okMsg( socket );
 		break;
 	case "Z":
 		ourChar.z = StringToNum( value );
 		break;
 	case "CANTRAIN":
 		if( ourChar.npc )
+		{
 			ourChar.trainer = (StringToNum( value ) == 1);
+			okMsg( socket );
+		}
 		break;
 	case "FROZEN":
 		ourChar.frozen = (StringToNum( value ) == 1);
+		okMsg( socket );
 		break;
 	case "VISIBLE":
 		ourChar.visible = StringToNum( value );
+		okMsg( socket );
 		break;
 	default:
 		if( !ourChar.SetSkillByName( uKey, StringToNum( value ) ) )
@@ -242,8 +300,10 @@ function onCallback1( socket, ourObj )
 {
 	var toOwn = socket.tempObj;
 	if( !socket.GetWord( 1 ) && ourObj.isChar && toOwn )
+	{
 		toOwn.owner = ourObj;
-
+		okMsg( socket );
+	}
 	socket.tempObj = null;
 }
 
@@ -263,6 +323,12 @@ function onCallback2( socket, ourObj )
 	{
 		var poisonStrength = socket.tempint;
 		ourObj.SetPoisoned( poisonStrength, 180000 );
+		okMsg( socket );
 	}
 	socket.tempint = 0;
+}
+
+function okMsg( socket )
+{ //Sends verification to the player that the specified value was successfully set.
+	socket.SysMessage( GetDictionaryEntry( 1756, socket.Language ));
 }
