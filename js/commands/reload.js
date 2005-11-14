b666f2f6b964b6de09cbce27478e283e69e4050d
Geir Ove Alnes <xuri@xoduz.org>
2005-11-14 04:07:52 +0000

    Added feedback message for the dupe-command 	Added commands: reloadalljs, reloadregions, reloadspawnregions, reloadspells, reloadcommands, reloadhtml, reloadall

--- js/commands/reload.js
+++ js/commands/reload.js
@@ -4,6 +4,13 @@ function CommandRegistration()
 	RegisterCommand( "reloaddefs", 3, true )	// reload all dfns
 	RegisterCommand( "reloadini", 3, true );	// reload uox.ini
 	RegisterCommand( "reloadjsfile", 3, true );	// reload JavaScript file
+	RegisterCommand( "reloadalljs", 3, true ); // reload all JavaScript files
+	RegisterCommand( "reloadregions", 3, true ); // reload regions
+	RegisterCommand( "reloadspawnregions", 3, true ); // reload spawnregions
+	RegisterCommand( "reloadspells", 3, true ); //reload spells
+	RegisterCommand( "reloadcommands", 3, true ); //reload commands
+	RegisterCommand( "reloadhtml", 3, true ); //reload HTML templates
+	RegisterCommand( "reloadall", 3, true ); //reload everything
 }
 
 function command_RELOADACCOUNTS( socket, cmdString )
@@ -32,4 +39,48 @@ function command_RELOADJSFILE( socket, cmdString )
 	var scriptID = StringToNum( cmdString );
 	socket.SysMessage( "Attempting Reload of JavaScript (ScriptID " + cmdString + ")" );
 	ReloadJSFile( scriptID );
+}
+
+function command_RELOADALLJS( socket, cmdString )
+{
+	socket.SysMessage( "Attempting Reload of all JavaScripts." );
+	Reload( 5 );
+}
+
+function command_RELOADREGIONS( socket, cmdString )
+{
+	socket.SysMessage( "Attempting Reload of Regions." );
+	Reload( 0 );
+}
+
+function command_RELOADSPAWNREGIONS( socket, cmdString )
+{
+	var dictMessage = GetDictionaryEntry( 68, socket.Language );
+	socket.SysMessage( dictMessage );
+	Reload( 1 );
+}
+
+function command_RELOADALL( socket, cmdString )
+{
+	var dictMessage = GetDictionaryEntry( 1746, socket.Language );
+	BroadcastMessage( dictMessage );
+	Reload( 8 );
+}
+
+function command_RELOADSPELLS( socket, cmdString )
+{
+	socket.SysMessage( "Reloading spells.." );
+	Reload( 2 );
+}
+
+function command_RELOADCOMMANDS( socket, cmdString )
+{
+	socket.SysMessage( "Reloading commands.." );
+	Reload( 3 );
+}
+
+function command_RELOADHTML( socket, cmdString )
+{
+	socket.SysMessage( "Reloading HTML Templates.." );
+	Reload( 6 );
 }
