9b10d325779dcfdb0856138ad72d2cc747834371
Scott Thompson <thompson.scott@live.com>
2006-02-01 19:53:37 +0000

    Committied Xuri's fix for the 'skin command

--- js/commands/skin.js
+++ js/commands/skin.js
@@ -11,7 +11,7 @@ function command_SKIN( socket, cmdString )
 		var value = StringToNum( cmdString );
 		var body = pUser.id;
 
-		if( ( value&0x4000 == 0x4000 ) && ( body >= 0x0190 && body <= 0x03E1 ) )
+		if( ( (value&0x4000) == 0x4000 ) && ( body >= 0x0190 && body <= 0x03E1 ) )
 			value = 0xF000;
 		if( value != 0x8000 )
 			pUser.colour = value;
