24dd62b6825373785764d870dc83d32cc1adda0f
Scott Thompson <thompson.scott@live.com>
2009-01-17 16:50:34 +0000

    Updated version to 0.98-4.0

--- source/cVersionClass.cpp
+++ source/cVersionClass.cpp
@@ -20,15 +20,15 @@ namespace UOX
 //o---------------------------------------------------------------------------o
 // product info
 //o---------------------------------------------------------------------------o
-const std::string VER				= "0.98-3";
-const std::string BUILD				= "7v";
+const std::string VER				= "0.98-4";
+const std::string BUILD				= "0";
 const std::string REALBUILD			= "0";
 const std::string SVER				= "2.0";
 const std::string CVER				= "2.0";
 const std::string IVER				= "2.0";
 const std::string PRODUCT			= "Ultima Offline eXperiment 3";
 const std::string NAME				= "The Official DevTeam";
-const std::string EMAIL				= "http://uox3.sourceforge.net/";
+const std::string EMAIL				= "http://openuo.sourceforge.net/";
 const std::string PROGRAMMERS		= "UOX3 DevTeam";
 
 //////////////////////////////////////////////////////////////////////
