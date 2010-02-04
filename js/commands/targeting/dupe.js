093c0e1ea4e099e112244115ef90552cef2534d0
Geir Ove Alnes <xuri@xoduz.org>
2010-02-04 20:43:00 +0000

    2/3/2010 - Xuri 	The duping command (dupe.js) should now be able to dupe spawner-objects properly 	Added missing Tinker Tools and Fletching Tools to jse_objectassociations.scp (Puck) 	Fixed an incorrect name-entry in namelists.dfn (Puck)

--- js/commands/targeting/dupe.js
+++ js/commands/targeting/dupe.js
@@ -15,12 +15,45 @@ function command_DUPE( socket, cmdString )
 
 function onCallback0( socket, ourObj )
 {
+	var dupeSuccess = false;
 	if( !socket.GetWord( 1 ) && ourObj && ourObj.isItem )
 	{
 		var numToDupe = socket.tempint;
 		for( var i = 0; i < numToDupe; ++i )
 		{
-			ourObj.Dupe( socket );
+			if( ourObj.isSpawner )
+			{
+				var pUser = socket.currentChar;
+				var newSpawner = CreateBlankItem( socket, pUser, 1, ourObj.name, ourObj.id, ourObj.colour, "SPAWNER", true )
+				if( ValidateObject( newSpawner ) )
+				{ 
+					newSpawner.spawnsection = ourObj.spawnsection;
+					if( ourObj.sectionalist )
+						newSpawner.sectionalist = true;
+					newSpawner.mininterval = ourObj.mininterval;
+					newSpawner.maxinterval = ourObj.maxinterval;
+					newSpawner.amount = ourObj.amount;
+					newSpawner.visible = ourObj.visible;
+					newSpawner.weight = ourObj.weight;
+					newSpawner.movable = ourObj.movable;
+					newSpawner.more = ourObj.more;
+					newSpawner.morex = ourObj.morex;
+					newSpawner.morey = ourObj.morey;
+					newSpawner.morez = ourObj.morez;
+					newSpawner.decayable = ourObj.decayable;
+					newSpawner.type = ourObj.type;
+					newSpawner.x = ourObj.x + 1;
+					newSpawner.y = ourObj.y + 1;
+					dupeSuccess = true;
+				}
+				else
+				{
+					socket.SysMessage( "Failed to dupe item." );
+					return;
+				}
+			}
+			else
+				ourObj.Dupe( socket );
 		}
 	}
 	socket.SysMessage( numToDupe + " duped items have been placed in your backpack." );
