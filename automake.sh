514851108741364a3909d680d98f9f2bd2513b8d
Xoduz <Xoduz@users.noreply.github.com>
2020-09-08 03:25:57 +0800

    Build process improvements
    Fixed grammar mistake in automake.sh that prevented compiling SpiderMonkey on MacOS (Xuri)
    Updated readme with extended compiling details in spoiler tags (Xuri)
    Undid static cast in jsapi.h (Xuri)
    Fixed issue with two different versions of cPlayerAction.cpp with different case in name causing issues (Xuri)
    The UOX3/shared/ directory will now be created automatically on startup if it doesn't already exist (punt)
    Updated README.md and UOX3 documentation with improved instructions for installing necessary build tools on Linux and how to copy files to working directory after compiling. (Xuri)
    Moved automake.sh from source directory to project root, and made it copy binary to project root directory after compiling (Xuri)

--- automake.sh
+++ automake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../spidermonkey
+cd spidermonkey
 make -f Makefile.ref DEFINES=-DHAVE_VA_LIST_AS_ARRAY CC=gcc
 
 if [ "$(uname)" = "Darwin" ]
@@ -16,3 +16,6 @@ then
 fi
 cd ../source
 make
+cp uox3 ..
+cd ..
+echo "Done! You should now find the compiled uox3 binary in the root UOX3 project directory. Copy this binary to a separate directory dedicated to running your UOX3 shard, along with the contents of the UOX3/data directory, to avoid potential git conflicts and accidental overwriting of data when pulling UOX3 updates in the future."
