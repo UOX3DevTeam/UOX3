780379f2a36427868faa84063e385f04e938477f
Xoduz <Xoduz@users.noreply.github.com>
2021-05-06 00:12:57 +0800

    Update Sort methods
    Updated Sort() methods in GenericList to use std::list member function sort instead of std::sort
    Applied some code style changes for consistency  :art:

--- source/GenericList.h
+++ source/GenericList.h
@@ -1,37 +1,37 @@
 #if !defined( __GENERICLIST_H__ )
 #define __GENERICLIST_H__
 //o-----------------------------------------------------------------------------------------------o
-//|	File		-	GenericList.h
-//|	Date		-	2021-05-04
+//|	File	-	GenericList.h
+//|	Date	-	2021-05-04
 //o-----------------------------------------------------------------------------------------------o
-//|	Purpose		-	Created as a counterpart of CDataList, based on std::list this class will behave
+//|	Purpose	-	Created as a counterpart of CDataList, based on std::list this class will behave
 //|						as expected when removing records in the middle of the array.
 //|
-//|				-	Version Historyz
+//|			-	Version Historyz
 //|
-//|						1.0		 		2021-05-04
-//|						Initial implementation.
+//|					1.0		 		2021-05-04
+//|					Initial implementation.
 //o-----------------------------------------------------------------------------------------------o
 #include <iostream>
 #include <cstdint>
-template < typename T >
+template <typename T>
 class GenericList
 {
 private:
-	typedef std::list< T >								GENERICLIST;
-	typedef typename std::list< T >::iterator			GENERICLIST_ITERATOR;
-	typedef typename std::list< T >::const_iterator		GENERICLIST_CITERATOR;
+	typedef std::list<T>							GENERICLIST;
+	typedef typename std::list<T>::iterator			GENERICLIST_ITERATOR;
+	typedef typename std::list<T>::const_iterator	GENERICLIST_CITERATOR;
 
 	GENERICLIST					objData;
 	GENERICLIST_ITERATOR		objIterator;
-	std::vector< GENERICLIST_ITERATOR >		objIteratorBackup;
+	std::vector<GENERICLIST_ITERATOR>		objIteratorBackup;
 
-	GENERICLIST_ITERATOR FindEntry(T toFind)
+	GENERICLIST_ITERATOR FindEntry( T toFind )
 	{
 		return std::find(objData.begin(), objData.end(), toFind);
 	}
 
-	bool Begin(void)
+	bool Begin( void )
 	{
 		return (objIterator == objData.begin());
 	}
@@ -42,81 +42,97 @@ public:
 		objIteratorBackup.resize(0);
 		objIterator = objData.end();
 	}
+
 	~GenericList()
 	{
 		objData.clear();
 		objIteratorBackup.clear();
 	}
 
-	T GetCurrent(void)
+	T GetCurrent( void )
 	{
 		T rvalue = NULL;
-		if (objIterator != objData.end())
+		if( objIterator != objData.end() )
+		{
 			rvalue = (*objIterator);
+		}
 		return rvalue;
 	}
 
-	T First(void)
+	T First( void )
 	{
 		objIterator = objData.end();
 		return Next();
 	}
-	T Next(void)
+
+	T Next( void )
 	{
 		T rvalue = NULL;
-		if (!Begin())
+		if( !Begin() )
 		{
 			--objIterator;
 			rvalue = (*objIterator);
 		}
 		else
+		{
 			objIterator = objData.end();
+		}
 		return rvalue;
 	}
-	bool Finished(void)
+
+	bool Finished( void )
 	{
 		return (objIterator == objData.end());
 	}
 
-	size_t Num(void) const
+	size_t Num( void ) const
 	{
 		return objData.size();
 	}
+
 	void Clear()
 	{
 		objData.clear();
 	}
-	bool Add(T toAdd)
+
+	bool Add( T toAdd )
 	{
-		if (FindEntry(toAdd) != objData.end())
+		if( FindEntry( toAdd ) != objData.end() )
+		{
 			return false;
+		}
 
 		const bool updateCounter = (objIterator == objData.end());
 		objData.push_back(toAdd);
-		if (updateCounter)
+		if( updateCounter )
+		{
 			objIterator = objData.end();
+		}
 
 		return true;
 	}
-	bool Remove(T toRemove, bool handleAlloc = false)
+
+	bool Remove( T toRemove, bool handleAlloc = false )
 	{
-		GENERICLIST_ITERATOR rIter = FindEntry(toRemove);
-		if (rIter != objData.end())
+		GENERICLIST_ITERATOR rIter = FindEntry( toRemove );
+		if( rIter != objData.end() )
 		{
-			if (objIterator != objData.end() && rIter == objIterator)
+			if( objIterator != objData.end() && rIter == objIterator )
 			{
 				++objIterator;
 			}
 
-			for (size_t q = 0; q < objIteratorBackup.size(); ++q)
+			for( size_t q = 0; q < objIteratorBackup.size(); ++q )
 			{
-				if (objIteratorBackup[q] != objData.end() && rIter == objIteratorBackup[q]) {
+				if( objIteratorBackup[q] != objData.end() && rIter == objIteratorBackup[q] )
+				{
 					++objIteratorBackup[q];
 				}
 			}
 			objData.erase(rIter);
 
-			if (handleAlloc) {
+			if (handleAlloc)
+			{
 				delete toRemove;
 			}
 			return true;
@@ -126,32 +142,36 @@ public:
 
 	void Pop(void)
 	{
-		if (!objIteratorBackup.empty()) {
+		if( !objIteratorBackup.empty() )
+		{
 			objIterator = objIteratorBackup.back();
-
 			objIteratorBackup.pop_back();
 		}
-		else {
+		else
+		{
 			objIterator = objData.end();
 		}
 	}
-	void Push(void)
+
+	void Push( void )
 	{
 		objIteratorBackup.push_back(objIterator);
 
 	}
-	void Sort(void)
+
+	void Sort( void )
 	{
-		std::sort(objData.begin(), objData.end());
+		objData.sort();
 
 	}
-	void Sort(bool Comparer(T one, T two))
+
+	void Sort( bool Comparer( T one, T two ))
 	{
-		std::sort(objData.begin(), objData.end(), Comparer);
+		objData.sort(Comparer);
 	}
 };
 
 
 
-#endif // __CDATALIST_H__
+#endif // __GENERICLIST_H__
 
