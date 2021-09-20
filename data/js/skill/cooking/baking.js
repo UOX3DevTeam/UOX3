9ac5a4ab627f796b696408c2a07bde84afa544f1
Malroth <arthenson67@gmail.com>
2021-09-20 13:03:52 -0500

    Cooking OverHaul
    Split food2.dfn into new  dfn files cooking.dfn/vegetables.dfn/meats.dfn/fruits.dfn
    
    Fixed pitcher of water  so it can be poured onto the  bag of flour and remove one content and set 20 contents for flour bag.
    
    Fixed four bag can be double clicked and target water and use one content of water.  and give it self 20 uses for dough.
    
    Added all dough baking
    Added all meat cooking.
    Added Heatsources
    Fixed Flourmill to have animation and give bag of flour when double clicked.
    cleaned up baking and cooking not every thing needed a skill check.

---
+++ data/js/skill/cooking/baking.js
@@ -0,0 +1,146 @@
+// baking script
+
+function onUseChecked ( pUser, iUsed )
+{
+	// get users socket
+	var srcSock = pUser.socket;
+
+	// is it in users pack?
+	if( iUsed.container != null )
+	{
+		var iPackOwner = GetPackOwner( iUsed, 0 );
+		if( iPackOwner.serial != pUser.serial )
+		{
+			srcSock.SysMessage( GetDictionaryEntry( 6022, srcSock.language )); // This has to be in your backpack before you can use it.
+		}
+		else
+			// let the user target the heat source
+			srcSock.tempObj = iUsed;
+			srcSock.CustomTarget( 0, GetDictionaryEntry( 6097, srcSock.language )); // what do you want to cook this on?
+	}
+	else
+		srcSock.SysMessage( GetDictionaryEntry( 6022, srcSock.language )); // This has to be in your backpack before you can use it.
+	return false;
+}
+
+function onCallback0( tSock, targSerial )
+{
+	Baking(tSock, targSerial)
+}
+
+function Baking(tSock, targSerial)
+{
+	var pUser = tSock.currentChar;
+	var iUsed = tSock.tempObj;
+	var StrangeByte   = tSock.GetWord( 1 );
+	var tileID = tSock.GetWord(17);
+	var foodColor = iUsed.colour;
+
+	if( tileID == 0 || ( StrangeByte == 0 && targSerial.isChar ))
+	{
+		tSock.SysMessage( GetDictionaryEntry( 6095, tSock.language ) + " " + iUsed.name + " " + GetDictionaryEntry( 6096, tSock.language ) ); // You cannot cook your foodname on that.
+		return;
+	}
+
+	if(TriggerEvent( 106, "Ovens", tileID ))
+	{
+		if (TriggerEvent( 103, "RangeCheck", tSock, pUser ))
+		{
+			tSock.SysMessage(GetDictionaryEntry(393, tSock.language)); // That is too far away.
+			return;
+		}
+
+		if( !pUser.CheckSkill( 13, 0, 140 ) )
+		{
+			tSock.SysMessage(GetDictionaryEntry( 6093, tSock.language ) + " " + iUsed.name + " " + GetDictionaryEntry( 6094, tSock.language ) ); // You burn the foodname to a crisp! It's ruined.
+			iUsed.id = 0x09F2;//burn food id
+			return;
+		}
+
+		if (pUser.GetTag("CookingDelay") == 1)
+		{
+			tSock.SysMessage(GetDictionaryEntry(6098, tSock.language) + " " + iUsed.name + " " + GetDictionaryEntry(6099, tSock.language)); // You can not cook this foodname why you are cooking something!
+			return;
+		}
+
+		switch (iUsed.name)
+		{
+			case "unbaked apple pie":
+				pUser.StartTimer(5000, 1, true);
+				break;
+			case "unbaked pumpkin pie":
+				pUser.StartTimer(5000, 2, true);
+				break;
+			case "unbaked meat pie":
+				pUser.StartTimer(5000, 3, true);
+				break;
+			case "unbaked fruit pie":
+				pUser.StartTimer(5000, 4, true);
+				break;
+			case "unbaked peach cobbler":
+				pUser.StartTimer(5000, 5, true);
+				break;
+			case "unbaked quiche":
+				pUser.StartTimer(5000, 6, true);
+				break;
+			case "cake mix":
+				pUser.StartTimer(5000, 7, true);
+				break;
+			case "cookie mix":
+				pUser.StartTimer(5000, 8, true);
+				break;
+		}
+		pUser.SetTag("CookingDelay", 1)
+		pUser.UseResource(1, iUsed.id, foodColor);
+		pUser.SoundEffect( 0x0057, true );
+		return;
+	}
+}
+
+function onTimer(pUser, timerID)
+{
+	var socket = pUser.socket;
+	switch (timerID)
+	{
+		case 1:
+			CreateDFNItem(socket, pUser, "baked_apple_pie", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 2:
+			CreateDFNItem(socket, pUser, "baked_pumpkin_pie", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 3:
+			CreateDFNItem(socket, pUser, "baked_meat_pie", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 4:
+			CreateDFNItem(socket, pUser, "baked_fruit_pie", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 5:
+			CreateDFNItem(socket, pUser, "baked_peach_cobbler", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 6:
+			CreateDFNItem(socket, pUser, "baked_quiche", 1, "ITEM", true);
+			socket.SysMessage("you baked a pie");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 7:
+			CreateDFNItem(socket, pUser, "0x09E9", 1, "ITEM", true);
+			socket.SysMessage("you baked a cake");
+			pUser.SetTag("CookingDelay", null)
+			break;
+		case 8:
+			CreateDFNItem(socket, pUser, "0x160B", 1, "ITEM", true);
+			socket.SysMessage("you baked a cookies");
+			pUser.SetTag("CookingDelay", null)
+			break;
+	}
+}
--- data/js/skill/cooking/flour.js
+++ data/js/skill/cooking/flour.js
@@ -1,71 +1,157 @@
 // cooking script
 // 17/06/2001 Yeshe; yeshe@manofmystery.org
 // 21/07/2003 Xuri; Updated/rewrote the script
-// Updated to use dictionary messages
-// use wheat : target flour mill : get flour
+// 14/06/2005 Xuri; Fixed the script :P
+// 25/07/2021 Updated to use dictionary messages
+// use flour : target water pitcher : get dough
 
 function onUseChecked ( pUser, iUsed )
 {
-	// get users socket
-	var srcSock = pUser.socket;
-
-	// is it in users pack?
-	if( iUsed.container != null )
+	var srcSock = pUser.socket;	// get users socket
+	if( iUsed.container != null )	// is it in users pack?
 	{
 		var iPackOwner = GetPackOwner( iUsed, 0 );
 		if( iPackOwner.serial != pUser.serial )
+		{
 			srcSock.SysMessage( GetDictionaryEntry( 6022, srcSock.language )); // This has to be in your backpack before you can use it.
+		}
 		else
 		{
-			var countOfResource = pUser.ResourceCount( 0x1EBD );	// item ID
-			if( countOfResource < 4 )
+			if( iUsed.id == 0x1039 || iUsed.id == 0x1045 )
 			{
-				srcSock.SysMessage( GetDictionaryEntry( 6080, srcSock.language )); // You do not have enough resources! You need 4 sheaves of wheat!
+				iUsed.id++;
+				if (!iUsed.GetTag("FlourLeft"))
+				{
+					iUsed.SetTag("FlourLeft", 20);
+				}
 			}
 			else
-				srcSock.CustomTarget( 0, GetDictionaryEntry( 6081, srcSock.language )); // Where do you wish to grind the wheat?
+			{
+				srcSock.tempObj = iUsed;
+				srcSock.CustomTarget( 0, GetDictionaryEntry( 6074, srcSock.language )); // Which pitcher of water to use?
+			}
 		}
 	}
 	else
 		srcSock.SysMessage( GetDictionaryEntry( 6022, srcSock.language )); // This has to be in your backpack before you can use it.
-
 	return false;
 }
 
-function onCallback0( tSock, targSerial )
+function onCallback0( tSock, myTarget )
 {
 	var pUser = tSock.currentChar;
+	var iUsed = tSock.tempObj;
 	var StrangeByte   = tSock.GetWord( 1 );
 	var targX	= tSock.GetWord( 11 );
 	var targY	= tSock.GetWord( 13 );
 	var targZ	= tSock.GetSByte( 16 );
-	var tileID	= tSock.GetWord( 17 );
-	if( tileID == 0 || ( StrangeByte == 0 && targSerial.isChar ))
-	{ //Target is a MapTile or a Character
-		tSock.SysMessage( GetDictionaryEntry( 6082, tSock.language )); // You cannot grind your wheat on that.
+	var tileID = tSock.GetWord(17);
+	if( tileID == 0 || ( StrangeByte == 0 && myTarget.isChar ))
+	{ //Target is a MapTile, or a Character
+		tSock.SysMessage( GetDictionaryEntry( 6075, tSock.language )); // That is not a pitcher of water
 		return;
 	}
-	// Target is a Dynamic or Static Item
-	if( tileID == 0x188b || tileID == 0x188c || ( tileID >= 0x1920 && tileID <= 0x1923 ) || ( tileID >= 0x1925 && tileID <= 0x1927 ) ||
-	( tileID >= 0x192c && tileID <= 0x192f ) || ( tileID >= 0x1931 && tileID <= 0x1933 ))
-	{	// In case its a flour mill
-		// check if its in range
-		if(( pUser.x > targX + 3 ) || ( pUser.x < targX - 3 ) || ( pUser.y > targY + 3 ) || ( pUser.y < targY - 3 ) || ( pUser.z > targZ + 10 ) || ( pUser.z < targZ - 10 ))
+	// Target is a Dynamic Item
+	if( StrangeByte == 0 )
+	{
+		// If target self, close the flour bag
+		if( myTarget == iUsed )
+		{
+			myTarget.id--;
+			return;
+		}
+		if( myTarget.id != 0x0FF8 && myTarget.id != 0x0FF9 && myTarget.id != 0x1f9d && myTarget.id != 0x1f9e ) // is the item of the right type?
+		{
+			tSock.SysMessage( GetDictionaryEntry( 6075, tSock.language )); // That is not a pitcher of water
+			return;
+		}
+		// Check if its in range
+		if( iUsed.container != null )
+		{
+			var iPackOwner = GetPackOwner( iUsed, 0 );
+			if( iPackOwner.serial != pUser.serial )
+			{
+				tSock.SysMessage( GetDictionaryEntry( 393, tSock.language )); // That is too far away.
+			}
+		}
+		else if( myTarget.isItemHeld )
 		{
 			tSock.SysMessage( GetDictionaryEntry( 393, tSock.language )); // That is too far away.
 			return;
 		}
-		// remove 4 sheaves of wheat
-		var iMakeResource = pUser.ResourceCount( 0x1EBD );// is there enough resources to use up to make it
-		if( iMakeResource < 4 )
+		else
 		{
-			tSock.SysMessage( GetDictionaryEntry( 6080, tSock.language )); // You do not have enough resources! You need 4 sheaves of wheat!
+			if(TriggerEvent( 103, "RangeCheck", tSock, pUser ))
+			{
+				tSock.SysMessage( GetDictionaryEntry( 393, tSock.language )); // That is too far away.
+				return;
+			}
+		}
+		// remove one flour
+		var iMakeResource = pUser.ResourceCount( 0x1045 );	// is there enough resources to use up to make it
+		if( iMakeResource < 1 )
+		{
+			var iMakeResource = pUser.ResourceCount( 0x1046 );	// is there enough resources to use up to make it
+			if( iMakeResource < 1 )
+			{
+				var iMakeResource = pUser.ResourceCount( 0x1039 );	// is there enough resources to use up to make it
+				if( iMakeResource <1 )
+				{
+					var iMakeResource = pUser.ResourceCount( 0x103a );	// is there enough resources to use up to make it
+					if( iMakeResource < 1 )
+					{
+						tSock.SysMessage( GetDictionaryEntry( 6076, tSock.language )); // There is not enough flour in your pack!
+						return;
+					}
+					else
+						var iID = 0x103a;
+				}
+				else
+					var iID = 0x1039;
+			}
+			else
+				var iID = 0x1046;
+		}
+		else
+			var iID = 0x1045;
+		FlourLeft = iUsed.GetTag( "FlourLeft" );
+		if( FlourLeft > 0 )
+		{
+			FlourLeft = FlourLeft - 1;
+			iUsed.SetTag("FlourLeft", FlourLeft);
+		}
+		else
+			iUsed.Delete();
+
+		pUser.SoundEffect( 0x0134, true );
+		// check the skill
+		if( !pUser.CheckSkill( 13, 1, 1000 ) )	// character to check, skill #, minimum skill, and maximum skill
+		{
+			tSock.SysMessage( GetDictionaryEntry( 6077, tSock.language )); // You tried to make dough but failed.
 			return;
 		}
-		pUser.UseResource( 4, 0x1EBD ); // uses up a resource (amount, item ID, item colour)
-		pUser.SoundEffect( 0x021e, true );
-		var itemMade = CreateDFNItem( tSock, pUser, "0x1045", 1, "ITEM", true ); // makes a sack of flour
-		tSock.SysMessage( GetDictionaryEntry( 6083, tSock.language )); // You grind some wheat and put a sack of flour in your pack!
-		return;
+
+	    UsesLeft = myTarget.GetTag("UsesLeft");
+		if (UsesLeft > 0)
+		{
+			UsesLeft = UsesLeft - 1;
+			myTarget.SetTag("UsesLeft", UsesLeft);
+		}
+		else
+		{
+			if (myTarget.id == 0x0FF8 || myTarget.id == 0x1f9e)
+				myTarget.id = 0x0FF7;
+			if (myTarget.id == 0x0ff9 || myTarget.id == 0x1f9d)
+				myTarget.id = 0x0FF6;
+			myTarget.SetTag("ContentsType", 1);
+			myTarget.SetTag("EmptyGlass", 3);
+			myTarget.SetTag("UsesLeft", 0);
+			myTarget.SetTag("ContentsName", "nothing");
+		}
+
+		var itemMade = CreateBlankItem( tSock, pUser, 1, "#", 0x103D, 0x0, "ITEM", true ); // makes a dough
+		tSock.SysMessage( GetDictionaryEntry( 6078, tSock.language )); // You make some dough.
 	}
+	else // Target is a static item
+		tSock.SysMessage( GetDictionaryEntry( 6079, tSock.language )); // You cannot use that for making dough.
 }
