450ff7a4ef08a0718fad1164db27bf648670c2c1
Charles Kerr <punt1959@users.noreply.github.com>
2022-02-25 07:55:28 -0500

    instrument update

--- source/EventTimer.hpp
+++ source/EventTimer.hpp
@@ -5,13 +5,30 @@
 #include <chrono>
 #include <cstdint>
 #include <string>
+#if defined(TIMER_ON)
+#define STRINGIFY(variable) #variable
+#define EVENT_TIMER_ON(varname) auto varname = EventTimer()
+#define EVENT_TIMER_NOW(varname,msg) varname.output(STRINGIFY(msg))
+#else
+#define EVENT_TIMER_ON(varname)
+#define EVENT_TIMER_NOW(varname,msg)
+#endif
+/*
+#define EVENT_TIMER_OFF ( #varnam, #msg) \
+#if defined(TIMER_ON) \
+varname.output("msg") ; \
+#endif
+
+*/
+
+
 //=========================================================
 class EventTimer {
 private:
 	std::chrono::high_resolution_clock::time_point _now ;
 public:
 	EventTimer() ;
-	auto elapsed() -> long long ;
+	[[maybe_unused]] auto elapsed() -> long long ;
 	auto output(const std::string &label)->void ;
 };
 #endif /* EventTimer_hpp */
