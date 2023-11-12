9c216ed8137dc8d2e5776a14acb0ab983f3a2602
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2023-11-11 20:35:26 -0600

    Christmas Items
    New Christmas.dfn file in misc
    Added Christmas to Add menu under Holidays
    Added christmas tree to House.dfn
    Added holidaypottedplant.js
    Added snowpile.js
    Added addondeedgump.js
    Added redeedaddon.js
    Moved all Holiday js items into a folder items/holdays
    Updated Candy.js
    Added in Chopping new Addons with axe.
    
    Updated onlogout with new settings for candy.

---
+++ data/js/item/holidays/candy.js
@@ -0,0 +1,119 @@
+function onUseChecked(pUser, iUsed)
+{
+	var socket = pUser.socket;
+	socket.tempObj = iUsed;
+
+	var soundNumbers = [0x03A, 0x03B, 0x03C];
+	var randomIndex = Math.floor( Math.random() * soundNumbers.length );
+	var randomSoundNumber = soundNumbers[randomIndex];
+	var Acidity = parseInt( pUser.GetTempTag( "Acidity" ));
+	var Toothach = parseInt( pUser.GetTempTag( "toothach" ));
+
+	if( iUsed.sectionID == "RedCandyCane" || iUsed.sectionID == "0x2bdd" || iUsed.sectionID == "0x2bde" || iUsed.sectionID == "GreenCandyCane" || iUsed.sectionID == "0x2bdf" || iUsed.sectionID == "0x2be0" )
+	{
+		if( Acidity == 30 ) 
+		{
+			socket.SysMessage( "The extreme pain in your teeth subsides." );
+			return;
+		}
+		else 
+		{
+			socket.SysMessage("test 1");
+			if( Acidity <= 30 ) 
+			{
+				pUser.SetTempTag( "Acidity", Acidity += 5 );
+			}
+
+			if ( Toothach == 0)
+			{
+				socket.SysMessage("test 2");
+				pUser.SetTempTag( "toothach", 1 );
+				pUser.StartTimer( 1000, 0, true );
+			}
+		}
+	}
+
+	if( iUsed.sectionID == "gingerbreadcookie" || iUsed.sectionID == "0x2be1" || iUsed.sectionID == "0x2be2" ) 
+	{
+		var rand = RandomNumber(0, 1);
+		if( rand == 1 )
+		{
+			const msg = [
+				"Noooo!",
+				"Please don't eat me... *whimper",
+				"Not the face!",
+				"Ahhhhhh! My foot’s gone!",
+				"Please. No! I have gingerkids!",
+				"No, no! I’m really made of poison. Really.",
+				"Run, run as fast as you can! You can't catch me! I'm the gingerbread man!"
+			];
+
+			var randomIndex = getRandomIndex( msg );
+			iUsed.TextMessage( msg[randomIndex] );
+		}
+		else
+		{
+			iUsed.Delete();
+		}
+
+	}
+	else 
+	{
+		iUsed.Delete();
+		socket.SysMessage("You feel as if you could eat as much as you wanted!");
+		pUser.SoundEffect(randomSoundNumber, false);
+	}
+}
+
+// Function to get a random index from an array
+function getRandomIndex( array ) 
+{
+	return Math.floor( Math.random() * array.length );
+}
+
+function onTimer( pUser, timerID ) 
+{
+	var socket = pUser.socket;
+	var Acidity = pUser.GetTempTag( "Acidity" );
+	const phrases = [
+		"ARRGH! My tooth hurts sooo much!",
+		"You just can't find a good Britannian dentist these days...",
+		"My teeth!",
+		"MAKE IT STOP!",
+		"AAAH! It feels like someone kicked me in the teeth!"
+	];
+
+	if( timerID == 0 )
+	{
+		if( pUser.GetTempTag( "toothach" ) == 1 )
+		{
+			if( Acidity == 30 ) 
+			{
+				socket.SysMessage( "The extreme pain in your teeth subsides." );
+				pUser.StartTimer( 1000, 1, true );
+				return;
+			}
+			if( Acidity > 0 )
+			{
+				pUser.SetTempTag("Acidity", Acidity -= 1);
+				var randomIndex = getRandomIndex(phrases);
+				pUser.TextMessage(phrases[randomIndex]);
+				pUser.StartTimer(9000, 0, true);
+			}
+			else
+			{
+				pUser.StartTimer(1000, 1, true);
+			}
+		}
+		else 
+		{
+			pUser.StartTimer( 1000, 1, true );
+		}
+	}
+
+	if( timerID == 1 ) 
+	{
+		pUser.SetTempTag( "toothach", 0 );
+		pUser.SetTempTag( "Acidity", 0 );
+	}
+}
---
+++ data/js/item/holidays/snowpile.js
@@ -0,0 +1,88 @@
+function onUseChecked( pUser, iUsed )
+{
+	var socket = pUser.socket;
+	if( pUser.visible == 1 || pUser.visible == 2 )
+	{
+		pUser.visible = 0;
+	}
+
+	if( socket && iUsed && iUsed.isItem )
+	{
+		//Check to see if it's locked down
+		if( iUsed.movable == 2 || iUsed.movable == 3 )
+		{
+			socket.SysMessage(GetDictionaryEntry( 774, socket.language )); // That is locked down and you cannot use it
+			return false;
+		}
+		var itemOwner = GetPackOwner( iUsed, 0 );
+		if( itemOwner == null || itemOwner.serial != pUser.serial ) 
+		{
+			pUser.SysMessage(GetDictionaryEntry( 1763, socket.language )); // // The item must be in your backpack to use it.
+			return false;
+		}
+		else if( pUser.isonhorse )
+		{
+			pUser.SysMessage(GetDictionaryEntry(2713, socket.language)); // You cannot use this while riding a mount.
+			return false;
+		}
+
+
+		if( pUser.GetTempTag( "snowpile" ) == null || pUser.GetTempTag( "snowpile" ) == 0 )
+		{
+			socket.tempObj = iUsed;
+			pUser.SetTempTag( "snowpile", 1 );
+
+			//the addition of the false-flag in the EmoteMessage below tells the server to only
+			//send the message to this character
+			pUser.SysMessage( "You carefully pack the snow into a ball..." ); //You carefully pack the snow into a ball...
+			pUser.CustomTarget(0);
+			pUser.StartTimer( 5000, 0, true );
+		}
+		else if( pUser.GetTempTag( "snowpile" ) == 1 )
+		{
+			pUser.SysMessage( "The snow is not ready to be packed yet.  Keep trying." ); // The snow is not ready to be packed yet.  Keep trying.
+		}
+		else
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+function onCallback0( socket, myTarget )
+{
+	var pUser = socket.currentChar;
+	var iUsed = socket.tempObj;
+	if( pUser.visible == 1 || pUser.visible == 2 ) 
+	{
+		pUser.visible = 0;
+	}
+
+	if( !socket.GetWord(1) && myTarget.isChar )
+	{
+		pUser.DoAction( 0x9 );
+		pUser.SoundEffect( 0x145, true );
+		DoMovingEffect( pUser.x, pUser.y, pUser.z + 15, myTarget.x, myTarget.y, myTarget.z + 3, 0x36E4, 0x7, 0x00, true, 0x480, 0 );
+		myTarget.SysMessage( "You have just been hit by a snowball!" ); // You have just been hit by a snowball!
+		pUser.SysMessage( "You throw the snowball and hit the target!" ); // You throw the snowball and hit the target!
+	}
+	else
+	{
+		socket.SysMessage( "You can only throw a snowball at something that can throw one back." ); // You can only throw a snowball at something that can throw one back.
+	}
+}
+
+function onTimer( pUser, timerID ) 
+{
+	var socket = pUser.socket;
+	if( pUser.visible == 1 || pUser.visible == 2 ) 
+	{
+		pUser.visible = 0;
+	}
+
+	if( timerID == 0 )
+	{
+		pUser.SetTempTag( "snowpile", null );
+	}
+}
