b08e6bd0bbdf587514079c6b0fc6bb3417d2816c
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2023-11-30 16:30:22 -0600

    Update 2

---
+++ data/js/commands/moongate.js
@@ -0,0 +1,52 @@
+// This command is used to enable/disable spawn regions in specific facets
+const facetList = [ "felucca", "trammel", "ilshenar", "malas", "tokuno", "termur" ];
+
+function CommandRegistration()
+{
+	RegisterCommand( "enablemoongates", 3, true );
+	RegisterCommand( "disablemoongates", 3, true );
+}
+
+function command_ENABLEMOONGATES( socket, cmdString )
+{
+	var pUser = socket.currentChar;
+	var facetNum = parseInt( cmdString );
+
+	if( !isNaN( facetNum ) && facetNum >= 0 )
+	{
+		SetMoongateFacetStatus( facetNum, true );
+		socket.SysMessage( "Moongates have been enabled for facet " + facetNum + " (" + facetList[facetNum] + ")" );
+	}
+	else
+	{
+		var cmdStringTemp = ( cmdString.replace( / /g, "" ));
+		var facetListIndex = facetList.indexOf( cmdStringTemp.toLowerCase() );
+		if( facetListIndex != -1 )
+		{
+			SetMoongateFacetStatus( facetListIndex, true );
+			socket.SysMessage( "Moongates have been enabled for facet " + facetListIndex + " (" + cmdString + ")" );
+		}
+	}
+}
+
+function command_DISABLEMOONGATES( socket, cmdString )
+{
+	var pUser = socket.currentChar;
+	var facetNum = parseInt( cmdString );
+
+	if( !isNaN( facetNum ) && facetNum >= 0 )
+	{
+		SetMoongateFacetStatus( facetNum, false );
+		socket.SysMessage( "Moongates have been disabled for facet " + facetNum + " (" + facetList[facetNum] + ")" );
+	}
+	else
+	{
+		var cmdStringTemp = ( cmdString.replace( / /g, "" ));
+		var facetListIndex = facetList.indexOf( cmdStringTemp.toLowerCase() );
+		if( facetListIndex != -1 )
+		{
+			SetMoongateFacetStatus( facetListIndex, true );
+			socket.SysMessage( "Moongates have been disabled for facet " + facetListIndex + " (" + cmdString + ")" );
+		}
+	}
+}
