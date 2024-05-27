863c0dc3fc397a0a075a6953dd2af209cf99977a
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2024-05-27 17:20:19 -0500

    Update Skills
    Cleanup functions

--- data/js/combat/special_moves.js
+++ data/js/combat/special_moves.js
@@ -15,18 +15,18 @@ function onSpecialMove(pUser, abilityID)
 	//Whirlwind Attack
 }
 
-// Define the checkSkillRequirement function outside RequiredSkill
-function checkSkillRequirement(pUser, requiredSkillLevel, requiredSkill, skillMessage, abilityID) 
+function checkSkillRequirement( pUser, requiredSkillLevel, requiredSkill, skillMessage, abilityID ) 
 {
-	if (pUser.skills[requiredSkill] < requiredSkillLevel)
+	if( pUser.skills[requiredSkill] < requiredSkillLevel )
 	{
-		pUser.SysMessage("You need " + skillMessage + " weapon skill to perform that attack");
-		DeactivateSpecialMove(pUser, abilityID)
+		pUser.SysMessage( "You need " + skillMessage + " weapon skill to perform that attack" );
+		DeactivateSpecialMove( pUser, abilityID );
 		return false;
 	}
 	return true;
 }
 
+// archery 31 swordsmansip 40 macefighting 41 fencing 42 wrestling 43
 // Define the RequiredSkill function
 function RequiredSkill( pUser, abilityID )
 {
@@ -46,17 +46,17 @@ function RequiredSkill( pUser, abilityID )
 		"0x0e85": { primary: 0, secondary: 5, reqSkill: 40 },
 		"0x0e86": { primary: 0, secondary: 5, reqSkill: 40 },	// pickaxe  // DoubleStrike, Disarm
 
-		"0x0E87": { primary: 0, secondary: 6, reqSkill: 41 },
-		"0x0E88": { primary: 0, secondary: 6, reqSkill: 41 },	// Pitchforks  // BleedAttack, Dismount
+		"0x0E87": { primary: 0, secondary: 6, reqSkill: 42 },
+		"0x0E88": { primary: 0, secondary: 6, reqSkill: 42 },	// Pitchforks  // BleedAttack, Dismount
 
 		"0x0E89": { primary: 0, secondary: 3, reqSkill: 41 },
 		"0x0E8A": { primary: 0, secondary: 3, reqSkill: 41 },	// Quarter Staves  // DoubleStrike, ConcussionBlow
 
-		"0x0EC2": { primary: 0, secondary: 0, reqSkill: 41 },
-		"0x0EC3": { primary: 0, secondary: 0, reqSkill: 41 },	// Cleavers  // BleedAttack, InfectiousStrike
+		"0x0EC2": { primary: 0, secondary: 0, reqSkill: 40 },
+		"0x0EC3": { primary: 0, secondary: 0, reqSkill: 40 },	// Cleavers  // BleedAttack, InfectiousStrike
 
-		"0x0EC4": { primary: 12, secondary: 0, reqSkill: 41 },
-		"0x0EC5": { primary: 12, secondary: 0, reqSkill: 41 },	// Skinning Knives  // ShadowStrike, BleedAttack
+		"0x0EC4": { primary: 12, secondary: 0, reqSkill: 40 },
+		"0x0EC5": { primary: 12, secondary: 0, reqSkill: 40 },	// Skinning Knives  // ShadowStrike, BleedAttack
 
 		"0x0F43": { primary: 1, secondary: 5, reqSkill: 40 },
 		"0x0F44": { primary: 1, secondary: 5, reqSkill: 40 },	// hatchets  // ArmorIgnore, Disarm
@@ -76,8 +76,8 @@ function RequiredSkill( pUser, abilityID )
 		"0x0F4D": { primary: 11, secondary: 6, reqSkill: 40 },
 		"0x0F4E": { primary: 11, secondary: 6, reqSkill: 40 },	// Bardiches  // ParalyzingBlow, Dismount
 
-		"0x0F4F": { primary: 3, secondary: 0, reqSkill: 40 },
-		"0x0F50": { primary: 3, secondary: 0, reqSkill: 40 },	// Crossbows  // ConcussionBlow, MortalStrike
+		"0x0F4F": { primary: 3, secondary: 0, reqSkill: 31 },
+		"0x0F50": { primary: 3, secondary: 0, reqSkill: 31 },	// Crossbows  // ConcussionBlow, MortalStrike
 
 		"0x0F51": { primary: 0, secondary: 12, reqSkill: 40 },
 		"0x0F52": { primary: 0, secondary: 12, reqSkill: 40 },	// Daggers  // InfectiousStrike, ShadowStrike
@@ -93,20 +93,20 @@ function RequiredSkill( pUser, abilityID )
 		"0x0F60": { primary: 1, secondary: 3, reqSkill: 40 },
 		"0x0F61": { primary: 1, secondary: 3, reqSkill: 40 },	// Longswords  // ArmorIgnore, ConcussionBlow
 
-		"0x0F62": { primary: 1, secondary: 11, reqSkill: 40 },
-		"0x0F63": { primary: 1, secondary: 11, reqSkill: 40 },	// Spears  // ArmorIgnore, ParalyzingBlow
+		"0x0F62": { primary: 1, secondary: 11, reqSkill: 42 },
+		"0x0F63": { primary: 1, secondary: 11, reqSkill: 42 },	// Spears  // ArmorIgnore, ParalyzingBlow
 
-		"0x0FB4": { primary: 4, secondary: 12, reqSkill: 40 },
-		"0x0FB5": { primary: 4, secondary: 12, reqSkill: 40 },	// Sledge hammers // CrushingBlow, ShadowStrike
+		"0x0FB4": { primary: 4, secondary: 12, reqSkill: 41 },
+		"0x0FB5": { primary: 4, secondary: 12, reqSkill: 41 },	// Sledge hammers // CrushingBlow, ShadowStrike
 
 		"0x13AF": { primary: 1, secondary: 0, reqSkill: 40 },
 		"0x13B0": { primary: 1, secondary: 0, reqSkill: 40 },	// War Axes // ArmorIgnore, BleedAttack
 
-		"0x13B1": { primary: 11, secondary: 0, reqSkill: 40 },
-		"0x13B2": { primary: 11, secondary: 0, reqSkill: 40 },	// Bows // ParalyzingBlow, MortalStrike
+		"0x13B1": { primary: 11, secondary: 0, reqSkill: 31 },
+		"0x13B2": { primary: 11, secondary: 0, reqSkill: 31 },	// Bows // ParalyzingBlow, MortalStrike
 
-		"0x13B3": { primary: 12, secondary: 6, reqSkill: 40 },
-		"0x13B4": { primary: 12, secondary: 6, reqSkill: 40 },	// Clubs // ShadowStrike, Dismount
+		"0x13B3": { primary: 12, secondary: 6, reqSkill: 41 },
+		"0x13B4": { primary: 12, secondary: 6, reqSkill: 41 },	// Clubs // ShadowStrike, Dismount
 
 		"0x13B5": { primary: 0, secondary: 11, reqSkill: 40 },
 		"0x13B6": { primary: 0, secondary: 1, reqSkill: 40 },	// Scimitars // DoubleStrike, ParalyzingBlow
@@ -114,17 +114,17 @@ function RequiredSkill( pUser, abilityID )
 		"0x13B9": { primary: 11, secondary: 4, reqSkill: 40 },
 		"0x13BA": { primary: 11, secondary: 4, reqSkill: 40 },	// Viking Swords // ParalyzingBlow, CrushingBlow
 
-		"0x13FC": { primary: 0, secondary: 6, reqSkill: 40 },
-		"0x13FD": { primary: 0, secondary: 6, reqSkill: 40 },	// Heavy Crossbows // MovingShot, Dismount
+		"0x13FC": { primary: 0, secondary: 6, reqSkill: 31 },
+		"0x13FD": { primary: 0, secondary: 6, reqSkill: 31 },	// Heavy Crossbows // MovingShot, Dismount
 
-		"0x13E3": { primary: 4, secondary: 12, reqSkill: 40 },
-		"0x13E4": { primary: 4, secondary: 12, reqSkill: 40 },	// Smith's Hammers // CrushingBlow, ShadowStrike
+		"0x13E3": { primary: 4, secondary: 12, reqSkill: 41 },
+		"0x13E4": { primary: 4, secondary: 12, reqSkill: 41 },	// Smith's Hammers // CrushingBlow, ShadowStrike
 
 		"0x13F6": { primary: 0, secondary: 5, reqSkill: 40 },
 		"0x13F7": { primary: 0, secondary: 5, reqSkill: 40 },	// Butcher Knives // InfectiousStrike,Disarm
 
-		"0x13F8": { primary: 3, secondary: 0, reqSkill: 40 },
-		"0x13F9": { primary: 3, secondary: 0, reqSkill: 40 },	// Gnarled Staves // ConcussionBlow,ForceOfNature
+		"0x13F8": { primary: 3, secondary: 0, reqSkill: 41 },
+		"0x13F9": { primary: 3, secondary: 0, reqSkill: 41 },	// Gnarled Staves // ConcussionBlow,ForceOfNature
 
 		"0x13FA": { primary: 0, secondary: 0, reqSkill: 40 },
 		"0x13FB": { primary: 0, secondary: 0, reqSkill: 40 },	// Large Battle Axes // WhirlwindAttack,BleedAttack
@@ -132,26 +132,26 @@ function RequiredSkill( pUser, abilityID )
 		"0x13FE": { primary: 0, secondary: 1, reqSkill: 40 },
 		"0x13FF": { primary: 0, secondary: 1, reqSkill: 40 },	// Katana // DoubleStrike,ArmorIgnore
 
-		"0x1400": { primary: 1, secondary: 0, reqSkill: 40 },
-		"0x1401": { primary: 1, secondary: 0, reqSkill: 40 },	// Kryss // ArmorIgnore,InfectiousStrike
+		"0x1400": { primary: 1, secondary: 0, reqSkill: 42 },
+		"0x1401": { primary: 1, secondary: 0, reqSkill: 42 },	// Kryss // ArmorIgnore,InfectiousStrike
 
-		"0x1402": { primary: 12, secondary: 0, reqSkill: 40 },
-		"0x1403": { primary: 12, secondary: 0, reqSkill: 40 },	// Short Spears // ShadowStrike,MortalStrike
+		"0x1402": { primary: 12, secondary: 0, reqSkill: 42 },
+		"0x1403": { primary: 12, secondary: 0, reqSkill: 42 },	// Short Spears // ShadowStrike,MortalStrike
 
-		"0x1404": { primary: 0, secondary: 5, reqSkill: 40 },
-		"0x1405": { primary: 0, secondary: 5, reqSkill: 40 },	// War Forks // BleedAttack,Disarm
+		"0x1404": { primary: 0, secondary: 5, reqSkill: 42 },
+		"0x1405": { primary: 0, secondary: 5, reqSkill: 42 },	// War Forks // BleedAttack,Disarm
 
-		"0x1406": { primary: 4, secondary: 0, reqSkill: 40 },
-		"0x1407": { primary: 4, secondary: 0, reqSkill: 40 },	// War Maces // CrushingBlow,MortalStrike
+		"0x1406": { primary: 4, secondary: 0, reqSkill: 41 },
+		"0x1407": { primary: 4, secondary: 0, reqSkill: 41 },	// War Maces // CrushingBlow,MortalStrike
 
-		"0x1438": { primary: 0, secondary: 4, reqSkill: 40 },
-		"0x1439": { primary: 0, secondary: 4, reqSkill: 40 },	// War Hammers // WhirlwindAttack,CrushingBlow
+		"0x1438": { primary: 0, secondary: 4, reqSkill: 41 },
+		"0x1439": { primary: 0, secondary: 4, reqSkill: 41 },	// War Hammers // WhirlwindAttack,CrushingBlow
 
-		"0x143A": { primary: 0, secondary: 3, reqSkill: 40 },
-		"0x143B": { primary: 0, secondary: 3, reqSkill: 40 },	// Mauls // DoubleStrike,ConcussionBlow
+		"0x143A": { primary: 0, secondary: 3, reqSkill: 41 },
+		"0x143B": { primary: 0, secondary: 3, reqSkill: 41 },	// Mauls // DoubleStrike,ConcussionBlow
 
-		"0x143C": { primary: 1, secondary: 0, reqSkill: 40 },
-		"0x143D": { primary: 1, secondary: 0, reqSkill: 40 },	// Hammer Picks // ArmorIgnore,MortalStrike
+		"0x143C": { primary: 1, secondary: 0, reqSkill: 41 },
+		"0x143D": { primary: 1, secondary: 0, reqSkill: 41 },	// Hammer Picks // ArmorIgnore,MortalStrike
 
 		"0x143E": { primary: 0, secondary: 3, reqSkill: 40 },
 		"0x143F": { primary: 0, secondary: 3, reqSkill: 40 },	// Halberds // WhirlwindAttack,ConcussionBlow
@@ -164,73 +164,74 @@ function RequiredSkill( pUser, abilityID )
 	};
 
 	// Get items in user's hands
-	var itemRHand = pUser.FindItemLayer(0x01);
-	var itemLHand = pUser.FindItemLayer(0x02);
+	var itemRHand = pUser.FindItemLayer( 0x01 );
+	var itemLHand = pUser.FindItemLayer( 0x02 );
 
 	// Check if either hand has an item
-	if (itemRHand != null)
+	if( itemRHand != null )
 	{
 		// Check item in the right hand
-		if (weaponTypes[itemRHand.sectionID])
+		if( weaponTypes[itemRHand.sectionID] )
 		{
 			var weapon = weaponTypes[itemRHand.sectionID];
 
-			if (abilityID == weapon.primary)
+			if( abilityID == weapon.primary )
 			{
-				return checkSkillRequirement(pUser, 700, weapon.reqSkill, "70", abilityID);
+				return checkSkillRequirement( pUser, 700, weapon.reqSkill, "70", abilityID );
 			}
-			else if (abilityID == weapon.secondary)
+			else if( abilityID == weapon.secondary )
 			{
-				return checkSkillRequirement(pUser, 900, weapon.reqSkill, "90", abilityID);
+				return checkSkillRequirement( pUser, 900, weapon.reqSkill, "90", abilityID );
 			}
 		}
 	}
-	else if (itemLHand != null)
+	else if( itemLHand != null )
 	{
 		// Check item in the left hand
-		if (weaponTypes[itemLHand.sectionID])
+		if( weaponTypes[itemLHand.sectionID] )
 		{
 			var weapon = weaponTypes[itemLHand.sectionID];
-			if (abilityID == weapon.primary)
+
+			if ( abilityID == weapon.primary )
 			{
-				return checkSkillRequirement(pUser, 700, weapon.reqSkill, "70", abilityID);
+				return checkSkillRequirement( pUser, 700, weapon.reqSkill, "70", abilityID );
 			}
-			else if (abilityID == weapon.secondary)
+			else if( abilityID == weapon.secondary )
 			{
-				return checkSkillRequirement(pUser, 900, weapon.reqSkill, "90", abilityID);
+				return checkSkillRequirement( pUser, 900, weapon.reqSkill, "90", abilityID );
 			}
 		}
 	}
-	else //checking if both hands null if so, it is a wrestling skill
+	else// checking if both hands null if so, it is a wrestling skill
 	{
-		if (abilityID == 5)
+		if( abilityID == 5 )
 		{
-			return checkSkillRequirement(pUser, 700, 43, "70");
+			return checkSkillRequirement( pUser, 700, 43, "70" );
 		}
-		else if (abilityID == 11)
+		else if( abilityID == 11 )
 		{
-			return checkSkillRequirement(pUser, 900, 43, "90");
+			return checkSkillRequirement( pUser, 900, 43, "90" );
 		}
 	}
 
 	return true;
 }
 
-function CheckMana(pUser, abilityID, requiredMana)
+function CheckMana( pUser, abilityID, requiredMana )
 {
-	var NextUse = pUser.GetTempTag("doubleMana");
+	var NextUse = pUser.GetTempTag( "doubleMana" );
 	var iTime = GetCurrentClock();
-	if (pUser.mana <= requiredMana)
+	if( pUser.mana <= requiredMana )
     {
-		pUser.TextMessage("You need " + requiredMana + " mana to perform that attack");
-        DeactivateSpecialMove(pUser.socket, abilityID);
+		pUser.TextMessage( "You need " + requiredMana + " mana to perform that attack" );
+        DeactivateSpecialMove( pUser.socket, abilityID );
         return false;
     }
     else
 	{
-		pUser.SetTempTag("doubleMana", iTime.toString());
-		var Delay = 3000;
-		if ((iTime - NextUse) < Delay)
+		pUser.SetTempTag( "doubleMana", iTime.toString() );
+		var Delay = 3000;// If abilitie is performed with 3 seconds of last abilitie it will then cost double mana
+		if(( iTime - NextUse ) < Delay )
 		{
 			pUser.mana -= requiredMana * 2;
 			return true;
@@ -257,42 +258,42 @@ function onCombatDamageCalc( pAttacker, pDefender, fightSkill, hitLoc )
         return 0;
 
     // Check if attacker has armor ignore enabled
-	if (abilityID == 1) // armorignore
+	if( abilityID == 1 ) // armorignore
     {
         // Armor Ignore ignores defense modifiers, but deals only 90% of potential damage
         damage *= 0.9;
 
-        if(fightSkill == 31) // Archery
+        if( fightSkill == 31 ) // Archery
         {
             // Cap damage from Armor Strike attack at 30 for archery weapons
-            if(damage > 30)
+            if( damage > 30 )
                 damage = 30;
         }
         else
 		{
             // For all othe rfighting skills, cap damage from Armor Strike at 35
-            if(damage > 35)
+            if( damage > 35 )
                 damage = 35;
         }
     }
-	else if (abilityID == 12)// shadowstrike
+	else if( abilityID == 12 )// shadowstrike
 	{
 		damage *= 1.25;
 	}
-	else if (abilityID == 4)// crushingblow
+	else if( abilityID == 4 )// crushingblow
 	{
 		damage *= 1.5;
 	}
-	else if (abilityID == 3)// ConcussionBlow
+	else if( abilityID == 3 )// ConcussionBlow
 	{	
 		if (pDefender.maxhp > 0) 
 		{
-			var hitsPercent = (pDefender.hp / pDefender.maxhp) * 100.0;
+			var hitsPercent = ( pDefender.hp / pDefender.maxhp ) * 100.0;
 
             var manaPercent = 0;
 
-			if (pDefender.maxmana > 0)
-				manaPercent = (pDefender.mana / pDefender.maxmana) * 100.0;
+			if( pDefender.maxmana > 0 )
+				manaPercent = ( pDefender.mana / pDefender.maxmana ) * 100.0;
 
 			damage += Math.min(Math.floor(Math.abs(hitsPercent - manaPercent) / 4), 20);
 		}
@@ -316,7 +317,7 @@ function onCombatDamageCalc( pAttacker, pDefender, fightSkill, hitLoc )
 
 function onAttack( pAttacker, pDefender )
 {
-	var abilityID = pAttacker.GetTempTag("abilityID");
+	var abilityID = pAttacker.GetTempTag( "abilityID" );
 
 	if (abilityID == 1) // armnor ignore
 	{
