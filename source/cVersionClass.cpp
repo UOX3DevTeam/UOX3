cfdec73db772b1d0efe2f1e0f65ba64a68135de1
strattond <dsstratton@outlook.com>
2025-04-10 07:11:42 +1000

    Bump version to trigger a build

--- source/cVersionClass.cpp
+++ source/cVersionClass.cpp
@@ -11,7 +11,7 @@
 // product info
 //o------------------------------------------------------------------------------------------------o
 const std::string VER				= "0.99";
-const std::string BUILD				= "6a";
+const std::string BUILD				= "6b";
 const std::string REALBUILD			= "0";
 const std::string SVER				= "2.0";
 const std::string CVER				= "2.0";
