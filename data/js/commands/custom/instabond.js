8484aa40847d3e4bca6a5b112de7981fd6a16390
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2025-05-20 23:08:46 -0500

    Command check

--- data/js/commands/custom/instabond.js
+++ data/js/commands/custom/instabond.js
@@ -10,28 +10,36 @@ function command_INSTABOND( socket, cmdString )
 
 function onCallback0( socket, ourObj )
 {
-	if( !ValidateObject(ourObj) || !ourObj.npc )
+	const petBondingEnabled = GetServerSetting( "PetBondingEnabled" );
+	if( petBondingEnabled > 0 )
 	{
-		socket.SysMessage( GetDictionaryEntry( 19325, socket.language ));//Invalid target.
-		return;
-	}
+		if( !ValidateObject(ourObj) || !ourObj.npc )
+		{
+			socket.SysMessage( GetDictionaryEntry( 19325, socket.language ));//Invalid target.
+			return;
+		}
+	
+		if( !ourObj.tamed )
+		{
+			socket.SysMessage( GetDictionaryEntry( 19326, socket.language ));// That creature must be tamed before it can be bonded.
+			return;
+		}
 
-	if( !ourObj.tamed )
+		if( ourObj.GetTag( "isBondedPet" ) == true)
+		{
+			socket.SysMessage( GetDictionaryEntry( 19327, socket.language ));// That pet is already bonded.
+			return;
+		}
+
+		ourObj.KillJSTimer( 42, 3107 );
+		ourObj.SetTag( "bondingStarted", null );
+		ourObj.SetTag( "bondingPlayer", null );
+		ourObj.SetTag( "isBondedPet", true );
+
+		socket.SysMessage( GetDictionaryEntry( 19328, socket.language ));//Pet has been instantly bonded!
+	}
+	else
 	{
-		socket.SysMessage( GetDictionaryEntry( 19326, socket.language ));// That creature must be tamed before it can be bonded.
-		return;
+		socket.SysMessage( GetDictionaryEntry( 19329, socket.language ));//Pet bonding is disabled on this server.
 	}
-
-	if( ourObj.GetTag( "isBondedPet" ) == true)
-	{
-		socket.SysMessage( GetDictionaryEntry( 19327, socket.language ));// That pet is already bonded.
-		return;
-	}
-
-	ourObj.KillJSTimer( 42, 3107 );
-	ourObj.SetTag( "bondingStarted", null );
-	ourObj.SetTag( "bondingPlayer", null );
-	ourObj.SetTag( "isBondedPet", true );
-
-	socket.SysMessage( GetDictionaryEntry( 19328, socket.language ));//Pet has been instantly bonded!
 }
