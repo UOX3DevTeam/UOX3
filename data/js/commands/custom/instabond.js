79346fa3d11fe66fdede821f30392a1b79adf28b
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2025-05-20 19:19:01 -0500

    Update instabond.js

--- data/js/commands/custom/instabond.js
+++ data/js/commands/custom/instabond.js
@@ -5,26 +5,26 @@ function CommandRegistration()
 
 function command_INSTABOND( socket, cmdString )
 {
-	socket.CustomTarget( 0, GetDictionaryEntry( 19324, pDropper.socket.language ));//Select a pet to instantly bond.
+	socket.CustomTarget( 0, GetDictionaryEntry( 19324, socket.language ));//Select a pet to instantly bond.
 }
 
 function onCallback0( socket, ourObj )
 {
 	if( !ValidateObject(ourObj) || !ourObj.npc )
 	{
-		socket.SysMessage( GetDictionaryEntry( 19325, pDropper.socket.language ));//Invalid target.
+		socket.SysMessage( GetDictionaryEntry( 19325, socket.language ));//Invalid target.
 		return;
 	}
 
 	if( !ourObj.tamed )
 	{
-		socket.SysMessage( GetDictionaryEntry( 19326, pDropper.socket.language ));// That creature must be tamed before it can be bonded.
+		socket.SysMessage( GetDictionaryEntry( 19326, socket.language ));// That creature must be tamed before it can be bonded.
 		return;
 	}
 
 	if( ourObj.GetTag( "isBondedPet" ) == true)
 	{
-		socket.SysMessage( GetDictionaryEntry( 19327, pDropper.socket.language ));// That pet is already bonded.
+		socket.SysMessage( GetDictionaryEntry( 19327, socket.language ));// That pet is already bonded.
 		return;
 	}
 
@@ -33,5 +33,5 @@ function onCallback0( socket, ourObj )
 	ourObj.SetTag( "bondingPlayer", null );
 	ourObj.SetTag( "isBondedPet", true );
 
-	socket.SysMessage( GetDictionaryEntry( 19328, pDropper.socket.language ));//Pet has been instantly bonded!
+	socket.SysMessage( GetDictionaryEntry( 19328, socket.language ));//Pet has been instantly bonded!
 }
